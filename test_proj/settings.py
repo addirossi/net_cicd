6a555152b32fd1f470fc8a4035b628db0a555d13
Adilet Asankojoev <addirossi@gmail.com>
2022-07-13 18:29:48 +0600

    Update settings.py

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -130,3 +130,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
